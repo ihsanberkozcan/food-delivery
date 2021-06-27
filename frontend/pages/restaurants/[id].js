c1be3ea0789dd54aa7249c6963ea97b11f93a74f
İhsan Berk Özcan <59116996+ihsanberkozcan@users.noreply.github.com>
2021-06-28 00:54:54 +0300

    QR code added

--- frontend/pages/restaurants/[id].js
+++ frontend/pages/restaurants/[id].js
@@ -1,24 +1,44 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { Header } from "../../components/Header";
+var QRCode = require("qrcode.react");
 export default function Restorant({ restaurant }) {
   if (restaurant.dishes) {
     return (
       <>
         <Header />
-        <ul className="flex flex-wrap px-5 md:px-20">
+        <div className="flex w-full justify-between px-5 md:px-20">
+          <div className="flex">
+            <div className="p-5 rounded-md shadow-xl border-2 bg-white w-96">
+              <img
+                className="object-cover h-80 md:h-48 w-full"
+                src={`${process.env.NEXT_PUBLIC_API_URL}${restaurant.image[0].url}`}
+              />
+            </div>
+            <div className="w-full px-3 py-5">
+              <h1 className="font-bold">{restaurant.name}</h1>
+              <p className="font-medium">{restaurant.description}</p>
+              <p className="font-light">{restaurant.province}</p>
+            </div>
+          </div>
+          
+          <QRCode className="mt-5"
+            value={`${process.env.NEXT_PUBLIC_URL}/restaurants/${restaurant.id}`}
+          />
+        </div>
+        <ul className="grid gap-x-4 gap-y-4  grid-cols-5 px-5 md:px-20 mt-5">
           {restaurant.dishes.map((dish) => (
-            <div
+            <li
               key={dish.id}
-              className="relative transition hover:scale-105 duration-300 ease-in-out rounded-md w-full md:w-64 p-5 shadow-xl border-2 m-1.5 mb-5 md:mb-2 bg-white"
+              className="relative transition hover:scale-105 duration-300 ease-in-out rounded-md w-full  p-5 shadow-xl border-2 bg-white"
             >
               <img
                 className="object-cover h-80 md:h-48 w-full"
                 src={`${process.env.NEXT_PUBLIC_API_URL}${dish.image[0].url}`}
               />
-              <li className="mt-3">{dish.name}</li>
-              <li className="pb-5">{dish.description}</li>
-            </div>
+              <div className="mt-3">{dish.name}</div>
+              <div className="pb-5">{dish.description}</div>
+            </li>
           ))}
         </ul>
       </>
